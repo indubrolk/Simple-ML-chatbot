d90e6e4258bd06edcb2f9253b5dbd9e152d380ab
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-03-31 02:22:12 +0530

    main.py updated

--- main.py
+++ main.py
@@ -14,3 +14,13 @@ from torch.utils.data import DataLoader, TensorDataset
 class ChatbotModel(nn.Module):
     def __init__(self, input_size, output_size):
         super(ChatbotModel, self).__init__()
+
+        self.fc1 = nn.Linear(input_size, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, output_size)
+        self.relu = nn.ReLu()
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
