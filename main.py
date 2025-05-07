b163935115996f23d1f8691d8edb35e5ee98944c
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-05-07 21:37:26 +0530

    main.py committed

--- main.py
+++ main.py
@@ -28,8 +28,9 @@ class ChatbotModel(nn.Module):
         x = self.dropout(x)
         x = self.fc3(x)
 
-    class ChatbotAssistant:
-        def __init__(self, intents_path, function_mappings = none):
+class ChatbotAssistant:
+
+    def __init__(self, intents_path, function_mappings = none):
             self.model = None
             self.intents_path = intents_path
 
@@ -43,8 +44,8 @@ class ChatbotModel(nn.Module):
             self.x = None
             self.y = None
 
-            @staticmethod
-            def tokenize_and_lemmatizer(text):
+    @staticmethod
+    def tokenize_and_lemmatizer(text):
                 lemmatizer = nltk.WordNetLemmatizer()
 
                 words = nltk.word_tokenize(text)
@@ -52,25 +53,51 @@ class ChatbotModel(nn.Module):
 
                 return words
 
-            @staticmethod
-            def bag_of_words(words, vocabulary):
-                return [1 if word in words else 0 for word in vocabulary]
+    @staticmethod
+    def bag_of_words(self, words):
+                return [1 if word in words else 0 for word in self.vocabulary]
 
-            def parse_intents(self):
-                lemmatizer = nltk.WordNetLemmatizer():
+    def parse_intents(self):
+        lemmatizer = nltk.WordNetLemmatizer():
 
-                if os.path.exists(self.intents_path):
-                    with open(self.intents_path, 'r') as f:
-                        intents_data = json.load(f)
+            if os.path.exists(self.intents_path):
+                with open(self.intents_path, 'r') as f:
+                    intents_data = json.load(f)
 
-                    intents = []
-                    intents_responses = []
-                    vocabulary = []
-                    documents = []
+                intents = []
+                intents_responses = []
+                vocabulary = []
+                documents = []
 
-                    for intent in intents_data['intents']:
-                        if intent['tag'] not intents:
-                            intents.append(intent['tag'])
+            for intent in intents_data['intents']:
+                if intent['tag'] not self.intents:
+                    self.intents.append(intent['tag'])
+                    self.intents_responses[intent['tag']] = intent['responses']
+
+            for pattern in intent['patterns']:
+                patterns_words = self.tokenize_and_lemmatize(pattern)
+                self.vocabulary.extend(pattern_words)
+                self.document.append((pattern_words, intent['tag']))
+
+                self.vocabulary = sorted(set(vocabulary))
+
+    def prepare_data(self):
+                bags = []
+                indices = []
+
+                for document in self.documents:
+                    words = document[0]
+                    bag = self.bag_of_words(words, self.vocabulary)
+
+                    intent_index = self.intents.index(document[1])
+                    bags.append(bag)
+                    indices.append(intent_index)
+
+                self.x = np.array(bags)
+                self.y = np.array(indices)
+
+
+
 
 
 
